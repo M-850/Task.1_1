cb94be122da49aba457341c630b92b870ca745cf
M-850 <mmr005@mail.ru>
2020-07-18 23:04:35 +0300

    Piano

---
+++ Piano_5/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Piano_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1048, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 175);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(38, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 482);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1106, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать заново";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(922, 643);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Процент правильных ответов";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(974, 449);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1173, 616);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1213, 500);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1213, 540);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            this.label8.Click += new System.EventHandler(this.Label8_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1103, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 31);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Уровень 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1103, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 31);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Уровень 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(831, 616);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "label9";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1103, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 31);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Уровень 3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(974, 540);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(831, 584);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(974, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 766);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Piano_5/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Piano_1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap piano;// изобразжения клавиш первой актавы
+        Bitmap note;//изображение нот
+        int all_note;//колчичество изображений нот на уровне
+        double percent_true;//процент правильных нажатий
+        int level_pointer;//количество очков 
+        Color c;//цвет пикселя
+        List<Note> notes = new List<Note>();//список нот
+        Random random_note;//случайного числа
+        int i;//номер случайной ноты
+        int pointer;//количество правильных ответов
+        int pointer_no;//количество неправильных ответов
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Парвильных нажатий:";
+            label2.Text = "Не правильных нажатий:";
+            label6.Text = "";
+            button3.Enabled = false;
+            button4.Enabled = false;
+            this.pointer = 0;
+            this.pointer_no = 0;
+            label7.Text = pointer.ToString(); 
+            label8.Text = pointer_no.ToString();
+            label9.Text = "";
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            percent_true = 0;
+            pictureBox2.Enabled = true;
+            pointer = 0;
+            pointer_no = 0;
+            label7.Text = pointer.ToString();
+            label8.Text = pointer_no.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            level_pointer = 10;
+           
+            piano = new Bitmap("Piano_keys_1.bmp");
+            pictureBox2.Image = piano;
+            notes.Add(new Note("The_first_octave_С.gif", "Piano_keys_C_true.bmp", "Piano_keys_C_false.bmp", "До", 255, 255, 255));
+            notes.Add(new Note("The_first_octave_D.gif", "Piano_keys_D_true.bmp", "Piano_keys_D_false.bmp", "Ре", 255, 255, 254));
+            notes.Add(new Note("The_first_octave_E.gif", "Piano_keys_E_true.bmp", "Piano_keys_E_false.bmp", "Ми", 255, 254, 255));
+            notes.Add(new Note("The_first_octave_F.gif", "Piano_keys_F_true.bmp", "Piano_keys_F_false.bmp", "Фа", 255, 254, 254));
+            notes.Add(new Note("The_first_octave_G.gif", "Piano_keys_G_true.bmp", "Piano_keys_G_false.bmp", "Соль", 254, 255, 255));
+            notes.Add(new Note("The_first_octave_A.gif", "Piano_keys_A_true.bmp", "Piano_keys_A_false.bmp", "Ля", 254, 255, 254));
+            notes.Add(new Note("The_first_octave_B.gif", "Piano_keys_B_true.bmp", "Piano_keys_B_false.bmp", "Си", 254, 254, 255));
+            all_note = 7;
+            random_note = new Random();
+            i = random_note.Next(0, all_note);
+            pictureBox1.Image = notes[i].img_note;
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void PictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Image = piano;
+            label1.Text = "Парвильных нажатий:";
+            label2.Text = "Не правильных нажатий:";
+           
+                c = piano.GetPixel(e.X, e.Y);
+                if (c.R == notes[i].R && c.G == notes[i].G && c.B == notes[i].B)
+                {
+                pointer++;
+                label7.Text = pointer.ToString();
+                label6.Text = "Нажата " + notes[i].name;
+                pictureBox2.Image = notes[i].img_piano_note_true;
+                random_note = new Random();
+                i = random_note.Next(0, all_note);
+                pictureBox1.Image = notes[i].img_note;
+                }
+               
+                else
+                {
+                pointer_no++;
+                label6.Text = "Вы выбрали не правильно ноту";
+                label8.Text = pointer_no.ToString();
+                for(int j = 0; j < all_note; j++)
+                {
+                    if(notes[j].R==c.R && notes[j].B == c.B && notes[j].G == c.G)
+                    {
+                       pictureBox2.Image = notes[j].img_piano_note_false;
+                    }
+                }
+            }
+                if(pointer == level_pointer)
+            { 
+                percent_true = 100 * (double)pointer / (double)(pointer + pointer_no);
+                label9.Text = "Процент правильных нажатий:" + percent_true.ToString() + "%";
+                if(percent_true >= 90)
+                {   if (level_pointer == 10)
+                    {
+                        button3.Enabled = true;
+                    }
+                else if(level_pointer == 15)
+                    {
+                        button4.Enabled = true;
+                    }
+                }
+                pictureBox2.Enabled = false;
+            }
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            level_pointer = 10;
+            pictureBox2.Image = piano;
+            pointer = 0;
+            pointer_no = 0;
+            label7.Text = pointer.ToString();
+            label8.Text = pointer_no.ToString();
+            notes = new List<Note>();
+            notes.Add(new Note("The_first_octave_С.gif", "Piano_keys_C_true.bmp", "Piano_keys_C_false.bmp", "До", 255, 255, 255));
+            notes.Add(new Note("The_first_octave_D.gif", "Piano_keys_D_true.bmp", "Piano_keys_D_false.bmp", "Ре", 255, 255, 254));
+            notes.Add(new Note("The_first_octave_E.gif", "Piano_keys_E_true.bmp", "Piano_keys_E_false.bmp", "Ми", 255, 254, 255));
+            notes.Add(new Note("The_first_octave_F.gif", "Piano_keys_F_true.bmp", "Piano_keys_F_false.bmp", "Фа", 255, 254, 254));
+            notes.Add(new Note("The_first_octave_G.gif", "Piano_keys_G_true.bmp", "Piano_keys_G_false.bmp", "Соль", 254, 255, 255));
+            notes.Add(new Note("The_first_octave_A.gif", "Piano_keys_A_true.bmp", "Piano_keys_A_false.bmp", "Ля", 254, 255, 254));
+            notes.Add(new Note("The_first_octave_B.gif", "Piano_keys_B_true.bmp", "Piano_keys_B_false.bmp", "Си", 254, 254, 255));
+            all_note = 7;
+            random_note = new Random();
+            i = random_note.Next(0, all_note);
+            pictureBox1.Image = notes[i].img_note;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            level_pointer = 15;
+            pictureBox2.Enabled = true;
+            pictureBox2.Image = piano;
+            pointer = 0;
+            pointer_no = 0;
+            percent_true = 0;
+            label9.Text = "";
+            label7.Text = pointer.ToString();
+            label8.Text = pointer_no.ToString();
+            notes = new List<Note>();
+            notes.Add(new Note("The_first_octave_С_level_2.png", "Piano_keys_C_true.bmp", "Piano_keys_C_false.bmp", "До", 255, 255, 255));
+            notes.Add(new Note("The_first_octave_#_С_level_2.png", "Piano_keys_#_C_true.bmp", "Piano_keys_#_C_false.bmp", "Диез-До", 0, 0, 0));
+            notes.Add(new Note("The_first_octave_D_level_2.png", "Piano_keys_D_true.bmp", "Piano_keys_D_false.bmp", "Ре", 255, 255, 254));
+            notes.Add(new Note("The_first_octave_#_D_level_2.png", "Piano_keys_#_D_true.bmp", "Piano_keys_#_D_false.bmp", "Диез-Ре", 0, 0, 1));
+            notes.Add(new Note("The_first_octave_E_level_2.png", "Piano_keys_E_true.bmp", "Piano_keys_E_false.bmp", "Ми", 255, 254, 255));
+            notes.Add(new Note("The_first_octave_F_level_2.png", "Piano_keys_F_true.bmp", "Piano_keys_F_false.bmp", "Фа", 255, 254, 254));
+            notes.Add(new Note("The_first_octave_#_F_level_2.png", "Piano_keys_#_F_true.bmp", "Piano_keys_#_F_false.bmp", "Диез-Фа", 0, 1, 0));
+            notes.Add(new Note("The_first_octave_G_level_2.png", "Piano_keys_G_true.bmp", "Piano_keys_G_false.bmp", "Соль", 254, 255, 255));
+            notes.Add(new Note("The_first_octave_#_G_level_2.png", "Piano_keys_#_G_true.bmp", "Piano_keys_#_G_false.bmp", "Диез-Соль", 0, 1, 1));
+            notes.Add(new Note("The_first_octave_A_level_2.png", "Piano_keys_A_true.bmp", "Piano_keys_A_false.bmp", "Ля", 254, 255, 254));
+            notes.Add(new Note("The_first_octave_#_A_level_2.png", "Piano_keys_#_A_true.bmp", "Piano_keys_#_A_false.bmp", "Диез-Ля", 1, 0, 0));
+            notes.Add(new Note("The_first_octave_B_level_2.png", "Piano_keys_B_true.bmp", "Piano_keys_B_false.bmp", "Си", 254, 254, 255));
+            all_note = 12;
+            random_note = new Random();
+            i = random_note.Next(0, all_note);
+            pictureBox1.Image = notes[i].img_note;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            level_pointer = 20;
+            pictureBox2.Enabled = true;
+            pictureBox2.Image = piano;
+            pointer = 0;
+            pointer_no = 0;
+            percent_true = 0;
+            label9.Text = "";
+            label7.Text = pointer.ToString();
+            label8.Text = pointer_no.ToString();
+            notes.Add(new Note("The_first_octave_b_D_level_3.png", "Piano_keys_#_C_true.bmp", "Piano_keys_#_C_false.bmp", "Бемоль-Ре", 0, 0, 0));
+            notes.Add(new Note("The_first_octave_b_E_level_3.png", "Piano_keys_#_D_true.bmp", "Piano_keys_#_D_false.bmp", "Бемоль-Ми", 0, 0, 1));
+            notes.Add(new Note("The_first_octave_b_G_level_3.png", "Piano_keys_#_F_true.bmp", "Piano_keys_#_F_false.bmp", "Бемоль-Соль", 0, 1, 0));
+            notes.Add(new Note("The_first_octave_b_A_level_3.png", "Piano_keys_#_G_true.bmp", "Piano_keys_#_G_false.bmp", "Бемоль-Ля", 0, 1, 1));
+            notes.Add(new Note("The_first_octave_b_B_level_3.png", "Piano_keys_#_A_true.bmp", "Piano_keys_#_A_false.bmp", "Бемоль-Си", 1, 0, 0));
+            all_note = 17;
+            random_note = new Random();
+            i = random_note.Next(0, all_note);
+            pictureBox1.Image = notes[i].img_note;
+
+        }
+
+        private void Label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Note
+    {
+        public Bitmap img_note;//изображение ноты
+        public Bitmap img_piano_note_true;//изображение клавиш первой октавы при правлильном нажатии данной ноты
+        public Bitmap img_piano_note_false;//изображение клавиш первой октавы при неправильном нажатии данной ноты
+        public string name;// название ноты
+        public int R;//красная состовляющяя 
+        public int G;//зелённая состовляющая
+        public int B;//синия состовляющая
+        public Note(string bmp_file_name,string bmp_piano_note_true, string bmp_piano_note_false, string name, int R, int G, int B)
+       {
+            this.R = R;
+            this.G = G;
+            this.B = B;
+            this.name = name;
+            img_note = new Bitmap(bmp_file_name);
+            img_piano_note_true = new Bitmap(bmp_piano_note_true);
+            img_piano_note_false = new Bitmap(bmp_piano_note_false);  
+       }
+    }
+}
